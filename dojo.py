b0e3d026abe89fb276670553759d10d5954f32c5
Lucas Dutra <ldutra98@gmail.com>
2021-02-08 22:13:39 -0300

    Meu nome é lucas

--- dojo.py
+++ dojo.py
@@ -2,6 +2,6 @@ print("Oi, esse é o dojo de git da equipe 1 de EPS/MDS!")
 
 print("Por favor, mude a variável 'nome' para o seu nome")
 
-nome = "Paulin"
+nome = "Lucas"
 
 print(f"Tudo certo {nome}! Pode seguir para o próximo passo do dojo!")
